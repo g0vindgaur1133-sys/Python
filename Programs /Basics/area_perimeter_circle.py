cd85d6474759588a107dacd33b4a339b2eb7e90e
g0vindgaur1133-sys <g0vindgaur1133@gmail.com>
2026-03-07 16:05:23 +0530

    Update and rename Programs /basics to Programs /Basics/area_perimeter_circle.py

---
+++ Programs /Basics/area_perimeter_circle.py
@@ -0,0 +1,15 @@
+# area and perimeter of circle
+pi = 3.14
+r = 2
+area = pi * r*r
+print(area)
+perimeter = 2* pi* r
+print(perimeter)
+
+# take input r from user and area and perimeter of circle
+pi = 3.14
+r = int(input())
+area = pi * r*r
+print(area)
+perimeter = 2* pi* r
+print(perimeter)
