8a0abb1a1da4a549d2b2a0735d1cd39d3547b0e2
Govind Gaur <g0vindgaur1133@gmail.com>
2026-03-09 16:46:46 +0530

    Create area_perimeter_triangle.py

---
+++ Programs /Basics/area_perimeter_triangle.py
@@ -0,0 +1,18 @@
+# area and perimeter of triangle
+a, b, c = 10, 10, 10
+base, h = 2, 3
+area = 0.5 *base*h
+print(area)
+perimeter = a+b+c
+print(perimeter)
+
+# take  b, h ,a, b,c from user and print area and perimeter of triangle
+a = int(input())
+b = int(input())
+c = int(input())
+base = int(input())
+h = int(input())
+area = 1/2*(base*h)
+print(area)
+perimeter = a+b+c
+print(perimeter)
