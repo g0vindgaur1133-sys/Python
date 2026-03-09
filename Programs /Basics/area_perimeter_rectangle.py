d53910338e10c724173025379adaff1a750ff35d
Govind Gaur <g0vindgaur1133@gmail.com>
2026-03-09 16:45:06 +0530

    Create area_perimeter_rectangle.py

---
+++ Programs /Basics/area_perimeter_rectangle.py
@@ -0,0 +1,15 @@
+# area and perimeter of rectangle
+l = 5
+b = 2
+area = l*b
+print(area)
+perimeter =2*(l+b)
+print(perimeter)
+
+# take input l and b from user and then print area and perimeter of rectangle
+l = int(input())
+b = int(input())
+area = l*b
+print(area)
+perimeter =2*(l+b)
+print(perimeter)
