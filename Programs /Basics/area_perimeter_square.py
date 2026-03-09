3ecf4554c4d499a613c98b42ef3d0a6d20e90067
Govind Gaur <g0vindgaur1133@gmail.com>
2026-03-09 16:45:44 +0530

    Create area_perimeter_square.py

---
+++ Programs /Basics/area_perimeter_square.py
@@ -0,0 +1,13 @@
+# area and perimeter of square
+side = 4
+area = side* side
+print(area)
+perimeter = 4*side
+print(perimeter)
+
+# take input side from user and then print area and perimeter 
+side = int(input())
+area = side* side
+print(area)
+perimeter = 4*side
+print(perimeter)
