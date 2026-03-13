351ad6cf5eea9890ad736777c3b493a0f6987374
Govind Gaur <g0vindgaur1133@gmail.com>
2026-03-13 14:22:54 +0530

    Create celsius_fahrenheit.py

---
+++ Programs /Basics/celsius_fahrenheit.py
@@ -0,0 +1,9 @@
+# celsius to fahrenheit
+C = 38
+F = (C * 9/5)+32
+print(F)
+
+# take input celsius and then convert celsius to fahrenheit
+celsius = int(input())
+fahrenheit = (celsius * 9/5)+32
+print(fahrenheit)
